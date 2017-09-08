12f174154b4f75a7d05006240d8a330b0ba829f8
datianshi <dsz0111@gmail.com>
2017-09-08 13:30:43 -0500

    scaling diegos

---
+++ tasks/scaling-diego/task.sh
@@ -0,0 +1,20 @@
+set -eu
+
+cf_resources=$(
+  jq -n \
+    --argjson diego_cell_instances $DIEGO_CELL_INSTANCES \
+    '
+    {
+      "diego_cell": { "instances": $diego_cell_instances }
+    }
+    '
+)
+
+om-linux \
+  --target https://$OPSMAN_DOMAIN_OR_IP_ADDRESS \
+  --username $OPS_MGR_USR \
+  --password $OPS_MGR_PWD \
+  --skip-ssl-validation \
+  configure-product \
+  --product-name cf \
+  --product-resources "$cf_resources"
